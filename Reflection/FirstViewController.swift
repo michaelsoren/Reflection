80ec61f850ca2b9d87939f7c3526af741aea9f0b
Michael LeMay <mjlemay@cs.uchicago.edu>
2019-03-05 11:15:16 -0600

    Initial Commit

---
+++ Reflection/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Reflection
+//
+//  Created by Michael LeMay on 3/5/19.
+//  Copyright © 2019 Steel Fruit Collective. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
